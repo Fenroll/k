b6b919d10a6d6cc37f9df183beced040cc18de48
Fenroll <sergey.2210.pavlov@gmail.com>
2026-05-12 23:25:21 +0300

    update

--- courses.generated.js
+++ courses.generated.js
@@ -1,4 +1,4 @@
-window.coursesVersion = "202605121mxw";
+window.coursesVersion = "202605121t29";
 window.courses = [
   {
     "id": "000001",
@@ -1335,4 +1335,4 @@ window.courses = [
     "_contentKey": "archived-000005"
   }
 ];
-window.buildTimestamp = "12.05.2026 21:13";
+window.buildTimestamp = "12.05.2026 23:25";
